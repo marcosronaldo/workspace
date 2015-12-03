350ffeb002b55cec00c6289ba746636cf46bab1d
Marcos Ronaldo <marcos.rpj2@gmail.com>
2015-12-02 22:26:26 -0200

    adjusts

--- noosfero_environment/setup_repos.sh
+++ noosfero_environment/setup_repos.sh
@@ -1,7 +1,7 @@
 mkdir ~/git
 cd ~/git
-git -c http.sslVerify=false clone http://softwarepublico.gov.br/gitlab/softwarepublico/softwarepublico.git
-git clone https://gitlab.com/noosfero/noosfero.git
+[[ -d softwarepublico ]] || git -c http.sslVerify=false clone http://softwarepublico.gov.br/gitlab/softwarepublico/softwarepublico.git
+[[ -d noosfero ]] || git clone https://gitlab.com/noosfero/noosfero.git
 
 ln -s softwarepublico/src/noosfero-spb/software_communities noosfero/plugins/
 ln -s softwarepublico/src/noosfero-spb/gov_user noosfero/plugins/
@@ -11,4 +11,5 @@ ln -s softwarepublico/src/noosfero-spb/noosfero-spb-theme noosfero/public/design
 git config --global user.name "Marcos Ronaldo"
 git config --global user.email marcos.rpj2@gmail.com
 
-sh ../config/setup_config.sh
+./../config/setup_config.sh
+
